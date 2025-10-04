a06038cf49f2f6f007a4ac0764122a9c1275006b
Peter <petersheelan@gmail.com>
2025-10-04 23:42:12 +0100

    feat. get the last position of a user input in int an array

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -6,6 +6,8 @@ public class NumberUtils {
     //• Parameters: An array of ints and a single int
     //• Returns: an int (the position)
     //Write a JavaDoc for this method.
+
+
     public static int getPos(int[] nums, int pos) {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == pos){
@@ -14,5 +16,18 @@ public class NumberUtils {
         }
         return -1;
     }
+//    Exercise 2:
+//    Write a static method called getLastPos() in NumberUtils that finds the last position of a supplied int in an array.
+//    • Parameters: An array of ints and a single int
+//    • Returns: an int (the position)
+public static int getLastPos(int[] nums, int lastPosition) {
+    for (int i = nums.length -1; i >= 0; i--){
+        if (nums[i] == lastPosition){
+            return i;
+        }
+        }
+return -1;
+    }
 }
+
 
