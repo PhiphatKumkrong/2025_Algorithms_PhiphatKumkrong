a7bea9c83009096b242f108e6ce4e4cb81b01dcf
Peter <petersheelan@gmail.com>
2025-10-05 05:18:09 +0100

    feat. adding subset method but only add check if the array A and B are null or equal to zero, also check if the subset that cannot be the same size as the original set.

--- src/main/java/utils/NumberUtils.java
+++ src/main/java/utils/NumberUtils.java
@@ -11,12 +11,12 @@ public class NumberUtils {
 
     /**
      * @param nums the array to searched
-     * @param pos the position that looking for
+     * @param pos  the position that looking for
      * @return the first position of a supplied int in an array
      */
     public static int getPos(int[] nums, int pos) {
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == pos){
+            if (nums[i] == pos) {
                 return i;
             }
         }
@@ -29,17 +29,17 @@ public class NumberUtils {
 
     /**
      *
-     * @param nums the array to be seached from input
+     * @param nums         the array to be seached from input
      * @param lastPosition looking for value that will be the last position
      * @return the last position of an array that have in supplied
      */
     public static int getLastPos(int[] nums, int lastPosition) {
-    for (int i = nums.length -1; i >= 0; i--){
-        if (nums[i] == lastPosition){
-            return i;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == lastPosition) {
+                return i;
+            }
         }
-        }
-return -1;
+        return -1;
     }
 
     //Exercise 3:
@@ -57,13 +57,13 @@ return -1;
      */
     public static boolean isIdentical(int[] nums1, int[] nums2) {
 
-        if (nums1 == null && nums2 == null){
+        if (nums1 == null && nums2 == null) {
             return true;
         }
-        if (nums1 == null ||  nums2 == null){
+        if (nums1 == null || nums2 == null) {
             return false;
         }
-        if (nums1.length != nums2.length){
+        if (nums1.length != nums2.length) {
             return false;
         }
         for (int i = 0; i < nums1.length; i++) {
@@ -84,32 +84,57 @@ return -1;
      *
      * @param nums1 first array may be null or may contain value
      * @param nums2 second array may be null or may contain value
-     * the two arrays will be sorted in place using Arrays.sort before comparing
+     *              the two arrays will be sorted in place using Arrays.sort before comparing
      * @return true if the both array contain the same data or different order, otherwise will be false
      */
 
-    public static  boolean isEqual(int[] nums1, int[] nums2) {
+    public static boolean isEqual(int[] nums1, int[] nums2) {
 
-    if (nums1 == null && nums2 == null){
-        return true;
-    }
-    if (nums1 == null ||  nums2 == null){
-        return false;
-    }
-    if (nums1.length != nums2.length){
-        return false;
-    }
-    Arrays.sort(nums1);
-    Arrays.sort(nums2);
-
-    for (int i = 0; i < nums1.length; i++) {
-        if (nums1[i] != nums2[i]) {
+        if (nums1 == null && nums2 == null) {
+            return true;
+        }
+        if (nums1 == null || nums2 == null) {
             return false;
         }
+        if (nums1.length != nums2.length) {
+            return false;
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] != nums2[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    return true;
-}
+    //    Write a static method called isSubset() in NumberUtils that determines if the first supplied arrays of ints is a subset of the second
+//    supplied array (all data from one array is found somewhere in the other).
+//    Note: A subset cannot contain the same number of elements as the original set.
+//•	Parameters: Two arrays of ints, (A, B) (your method should check if A is a subset of B)
+//•	Returns: a boolean
+
+    /**
+     *
+     * @param nums1 is subset A that could contain value or null
+     * @param nums2 is subset B that could contain value or null
+     * @return it will return true if every value in subset A are occurs at least the same frequency otherwise is will be false;
+     */
+    public static boolean isSubset(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return false;
+        }
+        if (nums1.length == 0 || nums2.length == 0) {
+            return false;
+        }
+        if (nums1.length >= nums2.length){
+            return false;
+        }
+        return true;
+    }
 }
 
 
