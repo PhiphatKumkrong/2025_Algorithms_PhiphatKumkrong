16146473907f01d8d547fb6ca3f137942718f340
Peter <petersheelan@gmail.com>
2025-11-07 00:02:19 +0000

    feat. Creating a method Called toString, adding validation for empty array and adding for loop to run through all an array position

---
+++ src/main/java/utils/BookUtils.java
@@ -0,0 +1,15 @@
+package utils;
+import entities.Book;
+public class BookUtils {
+
+public static String toString(Book[] books){
+    //check if the array is empty or not;
+    if (books == null || books.length == 0){
+        return "book is not found";
+    }
+
+
+    //Now we loop through all position in an array
+    for (int i = 0; i < books.length; i++){
+    }
+}Vaildation
